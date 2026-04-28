182af8aedbef436cc7556407cf1cae4602a4e188
Andre <andre.sousarj@hotmail.com>
2026-04-28 20:58:19 -0300

    nest+prisma

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ContatoModule } from './contato/contato.module';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [ContatoModule, UserModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
