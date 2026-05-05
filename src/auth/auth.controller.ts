38f62a4b306f1c63b0dcecf6311139e6b1186612
Andre Sousa <andre.sousarj@hotmail.com>
2026-05-05 10:03:44 -0300

    ajustando a autenticação

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
-@Controller('auth')
-export class AuthController {}
+@Controller('auth') // A rota será http://localhost:3000/auth
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Post('login') // A rota final: POST /auth/login
+  async login(@Body() loginDto: any) {
+    // Aqui chamamos o serviço que você já configurou
+    return this.authService.login(loginDto.email, loginDto.password);
+  }
+}
