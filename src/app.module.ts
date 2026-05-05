6948a8e2ce1cec9cfe841cf58af2cf79f1b724e1
ADS 2024.1N <36129382024.1N@senacrjedu.local>
2026-05-05 20:17:01 -0300

    fix2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ContatoModule } from './contato/contato.module';
@@ -7,7 +8,7 @@ import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [DatabaseModule, ContatoModule, UserModule, AuthModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule, ContatoModule, UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/database/prisma.service.ts
+++ src/database/prisma.service.ts
@@ -1,11 +1,12 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaLibSql } from '@prisma/adapter-libsql';
 import { PrismaClient } from '../../generated/prisma/client.js';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-  constructor() {
-    const adapter = new PrismaLibSql({ url: 'file:./prisma/dev.db' });
+  constructor(config: ConfigService) {
+    const adapter = new PrismaLibSql({ url: config.get<string>('DATABASE_URL')! });
     super({ adapter } as any);
   }
 
