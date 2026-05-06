e25c6610f67f0f1ec0f11c1ce44568c31a08bfc7
Andre <andre.sousarj@hotmail.com>
2026-05-06 18:39:24 -0300

    Commit branch separada para sem autenticação

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ContatoModule } from './contato/contato.module';
 import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [ContatoModule, UserModule, AuthModule],
+  imports: [DatabaseModule, ContatoModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/contato/contato.module.ts
+++ src/contato/contato.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ContatoService } from './contato.service';
 import { ContatoController } from './contato.controller';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [ContatoController],
   providers: [ContatoService],
 })
